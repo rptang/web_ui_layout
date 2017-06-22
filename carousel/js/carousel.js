42eaa8a9f59e65d403b0e2970020c10e5266528c
rptang <274931049@qq.com>
2017-06-22 09:25:01 +0800

    finish carousel function

--- carousel/js/carousel.js
+++ carousel/js/carousel.js
@@ -4,6 +4,8 @@
 ;(function ($) {
     var Carousel = function (poster) {
 
+        var self = this;
+
         //保存单个旋转木马对象
         this.poster = poster;
         this.posterItemMain = poster.find("ul.poster-list");
@@ -11,12 +13,13 @@
         this.prevBtn = poster.find("div.poster-prev-btn");
         this.posterItems = poster.find("li.poster-item");
         this.posterFirstItem = this.posterItems.first();
-        // if(this.posterItems.size()%2==0){
-        //     this.posterItemMain.append(this.posterItems.eq(0).clone());
-        //     this.posterItems = this.posterItemMain.children();
-        // };
-        // this.posterFirstItem  = this.posterItems.first();
-        // this.posterLastItem  = this.posterItems.last();
+        if(this.posterItems.size()%2==0){
+            this.posterItemMain.append(this.posterItems.eq(0).clone());
+            this.posterItems = this.posterItemMain.children();
+        };
+        this.posterFirstItem  = this.posterItems.first();
+        this.posterLastItem  = this.posterItems.last();
+        this.rotateFlag   = true;
 
         //默认配置参数
         this.setting = {
@@ -34,9 +37,104 @@
 
         //设置配置参数值
         this.setSettingValue();
+        this.setPosterPos();
+
+        //左旋转按钮
+        this.nextBtn .click(function(){
+            if(self.rotateFlag){
+                self.rotateFlag = false;
+                self.carouseRotate("left");
+            };
+        });
+        //右旋转按钮
+        this.prevBtn .click(function(){
+            if(self.rotateFlag){
+                self.rotateFlag = false;
+                self.carouseRotate("right");
+            };
+        });
+        //是否开启自动播放
+        if(this.setting.autoPlay){
+            this.autoPlay();
+            this.poster.hover(function(){
+                window.clearInterval(self.timer);
+            },function(){
+                self.autoPlay();
+            });
+
+        };
     };
 
     Carousel.prototype = {
+
+        autoPlay:function(){
+            var self = this;
+            this.timer = window.setInterval(function(){
+                self.nextBtn.click();
+            },this.setting.delay);
+
+        },
+
+        //旋转
+        carouseRotate:function(dir){
+            var _this_  = this;
+            var zIndexArr = [];
+            //左旋转
+            if(dir === "left"){
+                this.posterItems .each(function(){
+                    var self = $(this),
+                        prev = self.prev().get(0)?self.prev():_this_.posterLastItem,
+                        width = prev.width(),
+                        height =prev.height(),
+                        zIndex = prev.css("zIndex"),
+                        opacity = prev.css("opacity"),
+                        left = prev.css("left"),
+                        top = prev.css("top");
+                    zIndexArr.push(zIndex);
+                    self.animate({
+                        width:width,
+                        height:height,
+                        //zIndex:zIndex,
+                        opacity:opacity,
+                        left:left,
+                        top:top
+                    },_this_.setting.speed,function(){
+                        _this_.rotateFlag = true;
+                    });
+                });
+                //zIndex需要单独保存再设置，防止循环时候设置再取的时候值永远是最后一个的zindex
+                this.posterItems.each(function(i){
+                    $(this).css("zIndex",zIndexArr[i]);
+                });
+            }else if(dir === "right"){//右旋转
+                this.posterItems .each(function(){
+                    var self = $(this),
+                        next = self.next().get(0)?self.next():_this_.posterFirstItem,
+                        width = next.width(),
+                        height =next.height(),
+                        zIndex = next.css("zIndex"),
+                        opacity = next.css("opacity"),
+                        left = next.css("left"),
+                        top = next.css("top");
+                    zIndexArr.push(zIndex);
+                    self.animate({
+                        width:width,
+                        height:height,
+                        //zIndex:zIndex,
+                        opacity:opacity,
+                        left:left,
+                        top:top
+                    },_this_.setting.speed,function(){
+                        _this_.rotateFlag = true;
+                    });
+
+                });
+                //zIndex需要单独保存再设置，防止循环时候设置再取的时候值永远是最后一个的zindex
+                this.posterItems.each(function(i){
+                    $(this).css("zIndex",zIndexArr[i]);
+                });
+            };
+        },
 
         //设置配置参数值去控制基本的宽度高度。。。
         setSettingValue:function () {
@@ -63,15 +161,11 @@
                 zIndex:Math.ceil(this.posterItems.size()/2)
             });
 
-            this.posterItems.css({
+            this.posterFirstItem.css({
                 width:this.setting.posterWidth,
                 height:this.setting.posterHeight,
                 left:w,
                 top:0,
-                zIndex:Math.floor(this.posterItems.size()/2)
-            });
-
-            this.posterFirstItem.css({
                 zIndex:Math.floor(this.posterItems.size()/2)
             });
         },
@@ -84,7 +178,81 @@
             }else{
                 return {};
             };
-        }
+        },
+
+        //设置剩余的帧的位置关系
+        setPosterPos:function () {
+            var self = this;
+            var sliceItems = this.posterItems.slice(1);//取出剩余的帧
+            var sliceSize = sliceItems.size() / 2;
+            var rightSlices = sliceItems.slice(0,sliceSize);
+            var level = Math.floor(this.posterItems.size() / 2);
+            var leftSlices = sliceItems.slice(sliceSize);
+
+            //设置右边帧的位置关系和宽度高度top
+            var rw = this.setting.posterWidth;
+            var rh = this.setting.posterHeight;
+            var gap = ((this.setting.width-this.setting.posterWidth)/2)/level;
+
+            var firstLeft = (this.setting.width-this.setting.posterWidth)/2;
+            var fixOffsetLeft = firstLeft+rw;
+
+            rightSlices.each(function(i){
+                level--;
+                rw = rw *self.setting.scale;
+                rh = rh *self.setting.scale;
+                var j = i;
+                $(this).css({
+                    zIndex:level,
+                    width:rw,
+                    height:rh,
+                    opacity:1/(++j),
+                    left:fixOffsetLeft+(++i)*gap-rw,
+                    top:self.setVerticalAlign(rh)
+                });
+            });
+            //设置左边的位置关系
+            var lw = rightSlices.last().width(),
+                lh  =rightSlices.last().height(),
+                oloop = Math.floor(this.posterItems.size()/2);
+            leftSlices.each(function(i){
+                $(this).css({
+                    zIndex:i,
+                    width:lw,
+                    height:lh,
+                    opacity:1/oloop,
+                    left:i*gap,
+                    top:self.setVerticalAlign(lh)
+                });
+                lw = lw/self.setting.scale;
+                lh = lh/self.setting.scale;
+                oloop--;
+            });
+        },
+
+        //设置垂直排列对齐
+        setVerticalAlign:function(height){
+            var verticalType  = this.setting.verticalAlign,
+                top = 0;
+            if(verticalType === "middle"){
+                top = (this.setting.height-height)/2;
+            }else if(verticalType === "top"){
+                top = 0;
+            }else if(verticalType === "bottom"){
+                top = this.setting.height-height;
+            }else{
+                top = (this.setting.height-height)/2;
+            };
+            return top;
+        },
+
+        autoPlay:function(){
+            var self = this;
+            this.timer = window.setInterval(function(){
+                self.nextBtn.click();
+            },this.setting.delay);
+
+        },
     };
 
     Carousel.init = function(posters){
